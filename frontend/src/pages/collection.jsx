261743613e27aac78a62c8fc964e008d05fa85a0
Hossam Al-Maraghy <161968436+hossamalmaraghy@users.noreply.github.com>
2025-02-26 23:28:37 +0200

    Adding the logic of the Search Bar

--- frontend/src/pages/collection.jsx
+++ frontend/src/pages/collection.jsx
@@ -5,7 +5,7 @@ import Title from '../components/title';
 import ProductItem from "../components/productItem";
 
 const Collection = () => {
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -30,6 +30,11 @@ const Collection = () => {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+      
+    }
 
     if (category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
@@ -61,7 +66,7 @@ const Collection = () => {
 
   useEffect(() => {
     applyFilter();
-  },[category, Subcategory])
+  },[category, Subcategory, search, showSearch])
 
   useEffect(() => {
     sortProduct();
