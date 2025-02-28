754e9a35043c7efa1f7330b96db341fd6f692ee2
Hossam Al-Maraghy <161968436+hossamalmaraghy@users.noreply.github.com>
2025-02-28 12:26:39 +0200

    Adding Products to Cart by increment the count of orders in the cart section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,10 +11,13 @@ import Orders from "./pages/orders";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import SearchBar from "./components/searchBar";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
       <Navbar />
       <SearchBar />
       <Routes>
