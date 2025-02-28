73e9aabbd1d65116291cf6d50314b5d7af3f86ea
Hossam Al-Maraghy <161968436+hossamalmaraghy@users.noreply.github.com>
2025-02-28 10:09:11 +0200

    Adding Related Products file and display it in the product file/page

---
+++ frontend/src/components/relatedProducts.jsx
@@ -0,0 +1,42 @@
+import { useContext, useEffect, useState } from "react"
+import { ShopContext } from "../context/shopContext"
+import Title from "./title";
+import ProductItem from "./productItem";
+
+const RelatedProducts = ({category,subCategory}) => {
+
+  const { products } = useContext(ShopContext);
+  const [related, setRelated] = useState([]);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      // let productCopy = products.slice();
+
+      const productCopy = products.filter(
+        (item) => item.category === category && item.subCategory === subCategory
+      );
+
+      setRelated(productCopy.slice(0, 5));
+    }
+  }, [products, category, subCategory]);
+
+  return (
+    <div className="my-24">
+      <div className="text-center text-3xl py-2">
+        <Title text1={'RELATED'} text2={'PRODUCTS'} />
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {related.length > 0 ? (
+        related.map((item, index) => (
+          <ProductItem key={index} id={item._id} name={item.name} price={item.price} image={item.image}/>
+        ))
+      ) : (
+          <p className="text-center col-span-full text-gray-500">No Related Products</p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default RelatedProducts
--- frontend/src/pages/product.jsx
+++ frontend/src/pages/product.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/shopContext";
 import { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/relatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
@@ -73,28 +74,58 @@ const Product = () => {
 
           {/* Size Selection */}
           <div className="mt-5">
-          <p className="">Select Size</p>
-          <div className="flex gap-2">
-            {productData.sizes.map((item, index) => (
-              <button
-                onClick={() => setSize(item)}
-                className={`border py-2 px-4 bg-gray-100 ${
-                  item === size ? "border-green-600" : ""
-                }`}
-                key={index}>
-                {item}
-              </button>
-            ))}
+            <p className="mb-2 font-medium">Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item === size ? "border-green-600" : ""
+                  }`}
+                  key={index}>
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 mt-10">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original product.</p>
+            <p>Cash on delivery available.</p>
+            <p>Easy return and exhcange policy within 7 days.</p>
           </div>
         </div>
+      </div>
 
-        <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 mt-10">
-          ADD TO CART
-        </button>
-        <hr className="mt-8 sm:w-4/5" />
-
+      {/* Description & Reviews */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 px-6 py-6 text-sm text-gray-500">
+          <p className="">
+            An e-commerce platform that provides a user-friendly interface for
+            buying and selling products online through a web application or a
+            mobile app that allows users to create accounts, browse products,
+            add items to their cart, checkout, and make payments.
+          </p>
+          <p>
+            The brand typically display products or servicess along with
+            detailed information such as descriptions, images, and pricing
+            information to potential customers or users of the platform.
+          </p>
         </div>
       </div>
+
+      {/* Desiplay related products */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
+      
     </div>
   ) : (
     <div className="opacity-0"></div>
