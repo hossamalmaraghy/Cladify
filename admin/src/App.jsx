06de2e74cf6f08ba0ba39c7e12907ecfc8d5eab4
Hossam Al-Maraghy <161968436+hossamalmaraghy@users.noreply.github.com>
2025-03-10 07:39:50 +0200

    Admin Files

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/navbar'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar   />
+    </>
   )
 }
 
